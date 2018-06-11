260a4f04e2409bed82b8c45d83be82c046347cf7
Mateusz Nowakowski <mateusz.nowakowski@capgemini.com>
2018-06-11 17:15:44 +0200

    time comparison if statemant and empty method call

---
+++ src/main/java/pl/com/draft/time/EmptyMethodVsIfComparison.java
@@ -0,0 +1,35 @@
+package pl.com.draft.time;
+
+/**
+ * @author Mateusz Nowakowski, Capgemini Technology Services.
+ */
+public class EmptyMethodVsIfComparison {
+
+    public static void main( String[] args ){
+
+        SimpleClass simpleClassNotAssign = null;
+        SimpleClass simpleClassAssign = new SimpleClass();
+        SimpleClass simpleClassAssign2 = new SimpleClass();
+        long start, stop;
+
+        start=System.nanoTime();
+        if(simpleClassNotAssign != null){}
+        stop=System.nanoTime();
+        System.out.println("Czas wykonania (w nanosekundach): "+(stop-start));
+
+        start=System.nanoTime();
+        simpleClassAssign.doSomething();
+        stop=System.nanoTime();
+        System.out.println("Czas wykonania (w nanosekundach): "+(stop-start));
+
+
+        start=System.nanoTime();
+        if(simpleClassAssign2 != null){
+            simpleClassAssign2.doSomething();
+        }
+        stop=System.nanoTime();
+        System.out.println("Czas wykonania (w nanosekundach): "+(stop-start));
+    }
+
+
+}
---
+++ src/main/java/pl/com/draft/time/SimpleClass.java
@@ -0,0 +1,9 @@
+package pl.com.draft.time;
+
+/**
+ * @author Mateusz Nowakowski, Capgemini Technology Services.
+ */
+public class SimpleClass {
+
+    public void doSomething(){}
+}
