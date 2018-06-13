bee61cc1dd685d1b35e0f6d4b700418848a19cb1
manowako <mateusz.nowakowski@capgemini.com>
2018-06-13 10:45:01 +0200

    Template Method Design Pattern

---
+++ src/main/java/pl/com/ugeon/templatemethod/TemplatePatternDemo.java
@@ -0,0 +1,19 @@
+package pl.com.ugeon.templatemethod;
+
+import pl.com.ugeon.templatemethod.game.Cricket;
+import pl.com.ugeon.templatemethod.game.Football;
+import pl.com.ugeon.templatemethod.game.Game;
+
+/**
+ * @author Mateusz Nowakowski, Capgemini Technology Services.
+ */
+public class TemplatePatternDemo {
+    public static void main(String[] args) {
+
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
---
+++ src/main/java/pl/com/ugeon/templatemethod/game/Game.java
@@ -0,0 +1,23 @@
+package pl.com.ugeon.templatemethod.game;
+
+/**
+ * @author Mateusz Nowakowski, Capgemini Technology Services.
+ */
+public abstract class Game {
+    abstract void initialize();
+    abstract void startPlay();
+    abstract void endPlay();
+
+    //template method - connot override it by use final modificator
+    public final void play(){
+
+        //initialize the game
+        initialize();
+
+        //start game
+        startPlay();
+
+        //end game
+        endPlay();
+    }
+}
